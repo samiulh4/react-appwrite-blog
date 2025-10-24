43bb888df209698b7ed9cb92ae41c4babb7f0381
Md.Samiul Haque <samiulh4@gmail.com>
2025-10-24 23:53:24 +0600

    dyoloyment issues

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
-import Header from "../components/Header/Header"
-import Footer from "../components/Footer/Footer"
-import SidebarRight from "../components/Sidebar/SidebarRight"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import SidebarRight from "./components/Sidebar/SidebarRight"
 import { Outlet } from "react-router-dom"
 
 const Layout = () => {
