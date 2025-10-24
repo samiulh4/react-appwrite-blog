ddb809cae69ea399e563d5e9fe2fbca93c4fe5e2
Md.Samiul Haque <samiulh4@gmail.com>
2025-10-24 09:53:10 +0600

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-center text-3xl font-bold underline">This is app write and react app</h1>
+      <h1 className="text-center text-3xl font-bold underline">Md.Samiul Haque</h1>
     </>
   )
 }
