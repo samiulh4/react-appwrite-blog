949be622bc544f0c202e0ebd79b3fd51e64d56e4
Md.Samiul Haque <samiulh4@gmail.com>
2025-10-28 01:29:50 +0600

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -57,7 +57,7 @@ const Profile = () => {
     if (loading) {
         return (
             <div className="min-h-screen flex items-center justify-center">
-                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-500"></div>
+                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-green-500"></div>
             </div>
         );
     }
